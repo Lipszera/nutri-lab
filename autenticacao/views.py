d2420d0faa3f7e3dd651e6a4f8e6f6b132bb0aca
Lipszera <nothefallen@gmail.com>
2022-07-21 01:05:26 -0300

    Validação de formulário na página de Cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,9 +1,34 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils import password_is_valid
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def  cadastro(request):
-  return render(request, 'cadastro.html')
+  if request.method == "GET":
+    return render(request, 'cadastro.html')
+  elif request.method == "POST":
+    username = request.POST.get('usuario')
+    email = request.POST.get('email')
+    password = request.POST.get('senha')
+    confirm_password = request.POST.get('confirmar_senha')
+
+    if not password_is_valid(request, password, confirm_password):
+      return redirect('/auth/cadastro')
+
+    try:
+      user = User.objects.create_user(username=username,
+                                      email=email,
+                                      password=password,
+                                      is_active=False)
+      user.save()
+      messages.add_message(request, constants.SUCCESS, 'Cadastro concluído com sucesso')
+      return redirect('/auth/logar')
+    except:
+      messages.add_message(request, constants.ERROR, 'Erro do sistema')
+      return redirect('/auth/cadastro')
 
 def logar(request):
   return HttpResponse ("Você está na página de login")
